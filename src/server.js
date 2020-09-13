59020e59b80bf55ffb2c2816fb125e78a875fd68
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-09-13 12:14:44 -0600

    Intro to types, schema, input, and mutations

--- src/server.js
+++ src/server.js
@@ -10,16 +10,52 @@ import user from './types/user/user.resolvers'
 const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
+  // ! => means required
   const rootSchema = `
+    type Cat {
+      name: String
+      age: Int!
+      bestFriend: Cat
+    }
+
+    input CatInput {
+      name: String
+      age: Int!
+      bestFriend: CatInput
+    }
+
+    type Query {
+      myCat: Cat
+      hello: String
+    }
+
+    type Mutation {
+      newCat(input: CatInput!): Cat!
+    }
+
     schema {
       query: Query
     }
   `
+
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: {},
+    resolvers: { 
+      Query: { 
+        myCat() { 
+          return {
+            name: 'Garfield',
+            age: 5,
+            isAlive: true,
+          }
+        },
+        hello() {
+          return "bye!"
+        }
+      } 
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
